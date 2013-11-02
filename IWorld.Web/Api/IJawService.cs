89f594dde30f7b20d67e24ca58ad352fafe2efa4
lovevrlsq <lovevrlsq@gmail.com>
2013-11-02 22:27:00 +0800

    添加大白鲨游戏的服务

---
+++ IWorld.Web/Api/IJawService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using IWorld.Contract.Client;
+
+namespace IWorld.Web.Api
+{
+    /// <summary>
+    /// 定义大白鲨游戏的服务
+    /// </summary>
+    [ServiceContract]
+    public interface IJawService
+    {
+        [OperationContract]
+        MainOfJawResult GetMainOfJaw(string token);
+
+        [OperationContract]
+        List<LotteryOfJawResult> GetLotterys(string token);
+
+        [OperationContract]
+        OperateResult Bet(BettingOfJawImport import, string token);
+
+        [OperationContract]
+        OperateResult Revocation(string token);
+    }
+}
+
---
+++ IWorld.Web/Api/JawService.svc.cs
@@ -0,0 +1,36 @@
+﻿using IWorld.Contract.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace IWorld.Web.Api
+{
+    /// <summary>
+    /// 大白鲨游戏的服务
+    /// </summary>
+    public class JawService : IJawService
+    {
+        public MainOfJawResult GetMainOfJaw(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<LotteryOfJawResult> GetLotterys(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperateResult Bet(BettingOfJawImport import, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperateResult Revocation(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
